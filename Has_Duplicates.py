5db47df351fa1f72af1c3305f05173bd250f4189
Şamil Kardaş <87974874+smlkrds@users.noreply.github.com>
2022-04-24 11:26:27 +0300

    Update Has_Duplicates.py

--- Has_Duplicates.py
+++ Has_Duplicates.py
@@ -8,3 +8,18 @@ def has_duplicates() :
             print("False")
 
 has_duplicates()
+
+
+""" Or
+
+def has_duplicates() :
+    t = [1, 2, 3, 4, 5, 6]
+    d = dict()
+    for i in t :
+        d[i] = d.setdefault(i, 0)
+        d[i] += 1
+        if d.setdefault(i, 1) > 1 :
+            return "it has duplicate!"
+    return "nope"
+    
+print(has_duplicates()) ==> This is same thing but made with dictionary. """
